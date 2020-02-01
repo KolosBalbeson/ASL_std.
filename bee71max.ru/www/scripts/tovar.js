e01267719f9624dab03989535ea41d9943abc842
KolosBalbeson <zonaneka71@mail.ru>
2020-02-02 01:16:32 +0300

    История моего роста, как web-разработчика

---
+++ bee71max.ru/www/scripts/tovar.js
@@ -0,0 +1,63 @@
+
+$(function(){
+	$("input#tel").mask("+7 (999) 999-99-99", {placeholder: "7-(999)-999-99-99" });
+});
+
+
+var nbone = 1;
+
+$(document).ready ( function(){
+	$(".numb").keyup(function() {
+		option = $(".numb").val();
+		if (option <= sum) {
+			result = option * price;
+			$('#price').val(result);
+		}else{
+			$('.numb').val(nbone);
+			$('#price').val(price);
+		}
+	});
+});
+			
+function next(){
+	a = $(".numb").val();
+	if (a < sum) {
+		a++;
+		$('.numb').val(a);
+		$('#price').val(price * a);
+	}else {
+		$('.numb').val(nbone);
+		$('#price').val(price);
+	}
+}
+function back(){
+	a = $(".numb").val();
+	b = $('#price').val();
+	if (a > 1 & a < sum) {
+		a--;	
+		$('.numb').val(a);
+		$('#price').val(b - price);
+	}else{
+		$('.numb').val(nbone);
+		$('#price').val(price);
+	}
+}
+
+function oformlenie(){
+	// Imager
+	$('.sidebar').addClass('sidebar2');
+	$('.sidebar2').removeClass('sidebar');
+	// Imager
+	$('.content').addClass('content2');
+	$('.content2').removeClass('content');
+}
+
+function unoformlenie(){
+	// Imager
+	$('.sidebar2').addClass('sidebar');
+	$('.sidebar').removeClass('sidebar2');
+	// Imager
+	$('.content2').addClass('content');
+	$('.content').removeClass('content2');
+}
+$('.order').click(function() {alert('Благодарим вас за заказ!!'); });
